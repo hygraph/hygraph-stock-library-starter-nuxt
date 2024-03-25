e050eba0d128a3ebf9c5101c9fad8ce1264cdf45
Tim Benniks <tbenniks@gmail.com>
2024-03-25 09:21:29 +0100

    nicer design

--- pages/asset/[id].vue
+++ pages/asset/[id].vue
@@ -16,34 +16,40 @@ const dateString = computed(() => {
 });
 </script>
 <template>
-  <p class="mb-2">
+  <p class="mb-8">
     <nuxt-link to="/" class="underline text-xs uppercase"
       >Back to index</nuxt-link
     >
   </p>
-  <div class="mb-4 flex space-x-2 items-center">
-    <img
-      v-if="asset?.createdBy?.picture"
-      :src="asset?.createdBy?.picture"
-      class="w-6 rounded-full"
-      loading="lazy"
-      width="50"
-      height="50"
-    />
-    <p class="font-bold">{{ asset?.createdBy?.name }}</p>
-  </div>
+
   <h1 class="text-4xl mb-1 font-bold">{{ asset?.alt }}</h1>
-  <p class="text-sm text-gray-500 mb-2">
+  <p class="text-sm text-gray-500">
     {{ asset?.width }}x{{ asset?.height }}, {{ asset?.mimeType }}, {{ size
     }}<br />
     {{ dateString }}
   </p>
-  <a
-    :href="asset?.original"
-    target="_blank"
-    class="inline-block mb-6 text-sm bg-slate-800 text-slate-100 py-2 px-4 rounded-md uppercase hover:bg-slate-600"
-    >Download original</a
-  >
+
+  <div class="flex justify-between mb-4">
+    <div class="flex space-x-2 items-center">
+      <img
+        v-if="asset?.createdBy?.picture"
+        :src="asset?.createdBy?.picture"
+        class="w-6 rounded-full"
+        loading="lazy"
+        width="50"
+        height="50"
+      />
+      <p class="font-bold">{{ asset?.createdBy?.name }}</p>
+    </div>
+
+    <a
+      :href="asset?.original"
+      target="_blank"
+      class="block text-sm bg-slate-800 text-slate-100 py-2 px-4 rounded-md uppercase hover:bg-slate-600"
+      >Download original</a
+    >
+  </div>
+
   <figure class="relative">
     <img
       v-if="asset"
